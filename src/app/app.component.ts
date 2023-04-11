f58f522816b2439fbf50e3eb61fbd1f624b71916
Ruben Brugman <ruben.brugman@spendlab.com>
2023-04-11 16:52:46 +0200

    Added navigation structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rxjs-workshop';
+  lastAssignment = 11;
+  currentAssignment = 1;
+  private firstAssignment = 1;
+
+  get previousDisabled(): boolean {
+    return this.firstAssignment >= this.currentAssignment;
+  }
+
+  get nextDisabled(): boolean {
+    return this.lastAssignment <= this.currentAssignment;
+  }
+
+  onClickPrevious(): void {
+    this.currentAssignment--;
+  }
+
+  onClickNext(): void {
+    this.currentAssignment++;
+  }
 }
