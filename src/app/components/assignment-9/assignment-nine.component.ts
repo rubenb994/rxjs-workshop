435a465dd53a1ea547ed103dc6711e99f8b2d01a
Ruben Brugman <ruben.brugman@spendlab.com>
2023-04-12 11:39:48 +0200

    Relative imports

--- src/app/components/assignment-9/assignment-nine.component.ts
+++ src/app/components/assignment-9/assignment-nine.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { PokemonService } from 'src/app/services/pokemon.service';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-assignment-nine',
